35653763c12dc2cb3ebf03a17a5de8b06ffcd0ce
yiqing.luo <yiqing.luo@accenture.com>
2018-10-28 22:09:35 -0400

    'added_data'

---
+++ data.py
@@ -0,0 +1,22 @@
+def Articles():
+	articles = [{
+		'id': 1,
+		'title': 'Article One',
+		'body': 'Lorem ipsum dolor sit',
+		'author': 'Brad Stevens',
+		'create_date': '10-28-2018'
+	},
+	{
+		'id': 2,
+		'title': 'Article Two',
+		'body': 'Lorem ipsum dolor sit',
+		'author': 'Brad Beal',
+		'create_date': '10-28-2018'
+	},{
+		'id': 3,
+		'title': 'Article Three',
+		'body': 'Lorem ipsum dolor sit',
+		'author': 'Brad Pitt',
+		'create_date': '10-28-2018'
+	}]
+	return articles
--- main.py
+++ main.py
@@ -1,13 +1,82 @@
-from flask import Flask
+from flask import Flask, render_template, flash, redirect, url_for, session, logging, request
+from data import Articles
+from flask_mysqldb import MySQL
+from wtforms import Form, StringField, TextAreaField, PasswordField, validators
+from passlib.hash import sha256_crypt
+
+
 app = Flask(__name__)
+
+#CONFIG MySQL
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = '243589624@Louis'
+app.config['MYSQL_DB'] = 'myflaskapp'
+app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
+#Initialize MySQL
+mysql = MySQL(app)
+
+Articles = Articles()
 
 @app.route('/')
 def hello():
-	return "Hello World!"
+	return render_template('home.html')
 
-@app.route('/<name>')
-def hello_name(name):
-	return 'Hello {}!'.format(name)
+# @app.route('/<name>')
+# def hello_name(name):
+# 	return 'Hello {}!'.format(name)
+
+@app.route('/about.html')
+def about():
+	return render_template('about.html')
+
+@app.route('/articles.html')
+def articles():
+	return render_template('articles.html', articles=Articles)
+
+@app.route('/articles/<string:id>/')
+def article(id):
+	return render_template('article.html', id=id)
+
+class RegisterForm(Form):
+	name = StringField('Name', [validators.Length(min=1, max=50)])
+	username = StringField('Username', [validators.Length(min=4, max=25)])
+	email = StringField('Email', [validators.Length(min=6, max=50)])
+	password = PasswordField('Password',[
+		validators.DataRequired(),
+		validators.EqualTo('confirm', message='Passwords do not match')
+	])
+	confirm = PasswordField('Confirm Password')
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+	form = RegisterForm(request.form)
+	if request.method == 'POST' and form.validate(): 
+		name = form.name.data
+		email = form.email.data
+		username = form.username.data
+		password = sha256_crypt.encrypt(str(form.password.data))
+
+		#Create cursor
+		cur = mysql.connection.cursor()
+
+		cur.execute('INSERT INTO users(name, email, username, password) VALUES(%s, %s, %s, %s)', (name, email, username, password))
+
+		#Commmit to DB
+		mysql.connection.commit()
+
+		#Close connection
+		cur.close()
+
+		flash('You are now registered and can log in!', 'success')
+
+		redirect(url_for('index'))
+
+		return render_template('register.html', form=form)
+	return render_template('register.html', form=form)
+
+
 
 if __name__=='__main__':
-	app.run()
+	app.secret_key='secret123'
+	app.run(debug=True)
